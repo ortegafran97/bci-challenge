ce969a4f31980443b551e15ad7ed83ddaaea1ce0
Franco Ortega <ortegafran97@gmail.com>
2024-02-28 19:27:56 -0300

    Exceptions entities

---
+++ src/main/java/com/donks/bci/bcichallenge/exceptions/ExceptionMessage.java
@@ -0,0 +1,8 @@
+package com.donks.bci.bcichallenge.exceptions;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ExceptionMessage {
+    private String message;
+}
---
+++ src/main/java/com/donks/bci/bcichallenge/exceptions/NotValidException.java
@@ -0,0 +1,9 @@
+package com.donks.bci.bcichallenge.exceptions;
+
+public class NotValidException extends RuntimeException{
+
+    public NotValidException(String message){
+        super(message);
+    }
+
+}
